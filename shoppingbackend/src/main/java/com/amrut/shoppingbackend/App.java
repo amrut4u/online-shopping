9301ffccce74b1bac65b05ce2adda81b04f3ee20
Amrut Waghmare <amrut4u2010@gmail.com>
2019-10-17 17:22:55 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/amrut/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.amrut.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
